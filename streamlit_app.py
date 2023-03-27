527cf185ce914e1c554582f8003c118ac1bdb01b
Mukesh-bsp <129025869+Mukesh-bsp@users.noreply.github.com>
2023-03-26 23:59:43 -0400

    Update streamlit_app.py
    Added the Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Dinner')
+streamlit.title('Breakfast menu')
+streamlit.title('Bread and Jam')
+streamlit.title('Banana')
+streamlit.title('Coffee')
