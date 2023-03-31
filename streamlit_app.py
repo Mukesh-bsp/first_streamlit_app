7b9f7745ac82c9e13e54ac40fffadb25737ad05c
Mukesh-bsp <129025869+Mukesh-bsp@users.noreply.github.com>
2023-03-30 23:20:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Dinner')
-streamlit.header('Breakfast menu')
-streamlit.text('Bread and Jam')
-streamlit.text('Banana')
-streamlit.text('Coffee')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
